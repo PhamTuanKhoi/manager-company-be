72f724bde56dabf2165f7dd23333d660a744f163
HO <huynhanhpham734@gmail.com>
2023-02-15 09:27:34 +0700

    settup schema join part

--- src/join-part/join-part.module.ts
+++ src/join-part/join-part.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { JoinPartService } from './join-part.service';
 import { JoinPartController } from './join-part.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JoinPart, JoinPartSchema } from './schema/join-part.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: JoinPart.name, schema: JoinPartSchema },
+    ]),
+  ],
   controllers: [JoinPartController],
-  providers: [JoinPartService]
+  providers: [JoinPartService],
+  exports: [JoinPartService],
 })
 export class JoinPartModule {}
--- src/join-part/schema/join-part.schema.ts
+++ src/join-part/schema/join-part.schema.ts
@@ -1,1 +1,25 @@
-export class JoinPart {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Part } from 'src/part/schema/part.schema';
+import { User } from 'src/user/schema/user.schema';
+
+export type JoinPartDocument = HydratedDocument<JoinPart>;
+
+@Schema({
+  timestamps: true,
+})
+export class JoinPart {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: () => User })
+  joinor: User;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: () => Part })
+  part: Part;
+
+  @Prop()
+  role: string;
+
+  @Prop({ default: Date.now() })
+  date: number;
+}
+
+export const JoinPartSchema = SchemaFactory.createForClass(JoinPart);
